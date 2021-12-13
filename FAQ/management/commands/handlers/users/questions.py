c4f85097717e10dffce2da6600ee0a00d5d32e37
ZhvankoEgor <71092374+ZhvankoEgor@users.noreply.github.com>
2021-12-13 16:05:05 +0300

    Add files via upload
    Код обновлён с реализацией класса Keyboards

--- FAQ/management/commands/handlers/users/questions.py
+++ FAQ/management/commands/handlers/users/questions.py
@@ -1,38 +1,35 @@
-from contextlib import suppress
-
 from aiogram.dispatcher.filters import Command
 from aiogram.types import Message, CallbackQuery
-from aiogram.utils.exceptions import MessageNotModified
 
-from FAQ.management.commands.keyboards.inline.callback_datas import title_text
-from FAQ.management.commands.keyboards.inline.choice_buttons import title, dict_keyboards, dict_answers
+from FAQ.management.commands.keyboards.inline.keyboards import Keyboards
 from FAQ.management.commands._loader import dp
+
+# Создание экземпляра клавиатур
+kb = Keyboards()
 
 
 # Обработчик кнопки "Вернуться на главную", должен быть выше хендлера для обработки дополнительных вопросов
 @dp.callback_query_handler(text_contains="to_main")
 async def on_main(call: CallbackQuery):
-    with suppress(MessageNotModified):
-        await call.answer(cache_time=20)
-        await call.message.answer(text=title_text,
-                                  reply_markup=title)
-        await call.message.edit_reply_markup()
+    await call.answer(cache_time=20)
+    await call.message.answer(text=kb.title_text,
+                              reply_markup=kb.title)
+    await call.message.edit_reply_markup()
 
 
 # Обработчик дополнительных вопросов
 @dp.callback_query_handler(text_contains="applic")
 async def applicate_mes(call: CallbackQuery):
-    with suppress(MessageNotModified):
-        index = call.data.split(",")
-        answer = dict_answers[index[1]]
-        await call.answer(cache_time=20)
-        await call.message.answer(answer,
-                                  reply_markup=dict_keyboards[index[1]])
-        await call.message.edit_reply_markup()
+    index = call.data.split(",")
+    answer = kb.dict_answers[index[1]]
+    await call.answer(cache_time=20)
+    await call.message.answer(answer,
+                              reply_markup=kb.dict_keyboards[index[1]])
+    await call.message.edit_reply_markup()
 
 # Стартовая страница
 @dp.message_handler(Command("start"))
 async def show_items(message: Message):
-    await message.answer(text=title_text,
-                         reply_markup=title)
+    await message.answer(text=kb.title_text,
+                         reply_markup=kb.title)
 
