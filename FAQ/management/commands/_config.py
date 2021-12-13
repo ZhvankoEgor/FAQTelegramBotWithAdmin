efbf649880261ffedc7d73d896bed44a0c2e456a
ZhvankoEgor <71092374+ZhvankoEgor@users.noreply.github.com>
2021-12-13 15:59:38 +0300

    Delete _config.py
    Код перенесен в Loader

--- FAQ/management/commands/_config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-from FAQ.management.commands._sqliter import dbconnector
-
-# Указываем файл с токеном и базу данных
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-db = dbconnector("/media/sf_Projects/AdminForFAQ/db.sqlite3")
